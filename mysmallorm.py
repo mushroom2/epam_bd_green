869c45cc70d0d185847b47ab32db94a1acd2465b
avi.home <avi.upsale@gmail.com>
2018-05-03 20:48:25 +0300

    db integration cont

--- mysmallorm.py
+++ mysmallorm.py
@@ -2,28 +2,32 @@ import csv
 import sqlite3 as lite
 import sys
 con = lite.connect('db')
-
+cur = con.cursor()
 def add_row_interests(cat, id):
 
-    cur = con.cursor()
-    cur.execute("insert into Interests values ('{}', {})".format(cat, id))
-    con.commit()
+
+    cur.execute('''insert into "Interests"  values ('{}', {})'''.format(cat, id))
+
 
 
 def update_row_interests(cat, id):
-    cur = con.cursor()
-    cur.execute('''update Interests set "InterestName" = '{}', "InterestId" = {} where "InterestId" = {}'''.format(cat, id, id))
-    con.commit()
+
+    cur.execute('''update "Interests" set "InterestName" = '{}', "InterestId" = {} where "InterestId" = {}'''.format(cat, id, id))
+
+
 
 
 with open('user_data_full_list.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
+    dialect = csv.Sniffer().sniff(csvfile.read(1024))
+    csvfile.seek(0)
+    reader = csv.reader(csvfile, dialect)
+    for row in reader:
+        print(row)
         if row[0] != 'Category':
             try:
-                add_row_interests(row[0], row[1])
+                add_row_interests(row[0].replace('"', ''), row[1])
             except lite.IntegrityError:
-                update_row_interests(row[0], row[1])
+                update_row_interests(row[0].replace('"', ''), row[1])
 
-
+con.commit()
 con.close()
