cf25ef96dc350b10ad4bc5fca7d9e043df1e3ac7
avi.home <avi.upsale@gmail.com>
2018-05-20 18:19:27 +0300

    app upd

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__, static_url_path='')
 
 
 @app.route("/")
-def hello():
-    return "Someapp"
+def index():
+    return render_template('index.html')
 
+
+@app.route("/poi/get")
+def get_poi_by_coords(request):
+    print(request.data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
