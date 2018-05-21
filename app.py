6bd8d82a94a509b08b81616ce9f669e42874f6b4
avi.home <avi.upsale@gmail.com>
2018-05-21 19:33:31 +0300

    form base

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__, static_url_path='')
 
@@ -8,9 +8,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/poi/get")
-def get_poi_by_coords(request):
-    print(request.data)
+@app.route("/poi/get", methods=['GET', 'POST'])
+def get_poi_by_coords():
+    if request.method == 'POST':
+        print(request.get_json(force=True))
+    return 'ttt'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
