28db2f3d94645148f49a7de900b61a2e11f2b893
Bohdan Makar <Bohdan_Makar@epam.com>
2018-06-13 01:23:05 +0300

    fixed config of kafka

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,9 +1,11 @@
 from kafka import KafkaProducer
 
-config = {'bootstrap_servers': 'sandbox-hdp.hortonworks.com:6667'}
+config = {'bootstrap_servers': 'PLAINTEXT://sandbox-hdp.hortonworks.com:6667', 'api_version': (0, 10)}
 producer = KafkaProducer(**config)
 
 
 def write_to_topic(storage):
     for poi in storage:
         producer.send('kafka-connect', {poi: storage[poi]})
+        producer.flush()
+    producer.close()
