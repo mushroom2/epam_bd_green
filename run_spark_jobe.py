33369f0ca3dbcb2f30980698a1ac1240e6a1bbc5
avi.home <avi.upsale@gmail.com>
2018-06-14 00:53:11 +0300

    ssh run spark job update network fix?

--- run_spark_jobe.py
+++ run_spark_jobe.py
@@ -9,6 +9,8 @@ def run_sj():
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=host, username=user, password=secret, port=port)
+    stdin, stdout, stderr = client.exec_command('source /opt/rh/python27/enable')
+    data = stdout.read() + stderr.read()
     stdin, stdout, stderr = client.exec_command('spark-context ./spark1.py')
     data = stdout.read() + stderr.read()
     print(data)
